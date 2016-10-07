881a5d639943aa05443c8064ab74a00c4dab9742
yoshizaki <yoshizaki@yoshizaki-no-MacBook-Pro.local>
2016-10-08 01:57:28 +0900

    [change] ファイルの変更

---
+++ dropdown/plactice.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".menu").click(function(){
+        $(".menu1").toggleClass("menuclick1")
+    });
+    $(".menu").click(function(){
+        $(".menu2").toggleClass("menuclick2")
+    });
+    $(".menu").click(function(){
+        $(".menu3").toggleClass("menuclick3")
+    });
+});
